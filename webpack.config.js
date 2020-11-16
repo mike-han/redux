029036d50945d6a883372035f779bbd452c40022
Mingyang Han <mingyang_han@esri.com>
2020-11-16 14:36:30 +0800

    Merge webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,55 @@
-const redux = require('./webpack/webpack.config.redux');
-const index = require('./webpack/webpack.config.index');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {
+  merge
+} = require("webpack-merge");
+const path = require('path');
+
+const common = {
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-react']
+        }
+      }
+    }],
+  },
+  devtool: 'inline-cheap-module-source-map'
+};
+
+const redux = merge(common, {
+  entry: path.resolve(__dirname, './src/redux/index'),
+  output: {
+    filename: 'redux.js',
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, './dist'),
+    library: 'redux',
+    libraryTarget: 'umd',
+  }
+});
+
+const index = merge(common, {
+  entry: path.resolve(__dirname, './src/index.jsx'),
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, './dist')
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    port: 9000
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  externals:{
+    redux: 'redux'
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: 'src/index.html'
+  })]
+})
 
 module.exports = [index, redux];
